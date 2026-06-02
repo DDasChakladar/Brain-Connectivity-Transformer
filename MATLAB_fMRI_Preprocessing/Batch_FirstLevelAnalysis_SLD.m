2fb693aaff521a8fd8d539e8b33393bdde018f92
Debashis Das Chakladar <40134571+DDasChakladar@users.noreply.github.com>
2026-06-02 16:46:16 +0530

    Delete MATLAB_fMRI_Preprocessing directory

--- MATLAB_fMRI_Preprocessing/Batch_FirstLevelAnalysis_SLD.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'C:\Debashis\Research\AAAI_2025\fMRI_DDSA_Denmark_Dataset\fMRI_DDSA_Denmark_Dataset\Prediction_Analysis\Batches\Batch_NonADHD_FirstLevelAnalysis_SLD_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
